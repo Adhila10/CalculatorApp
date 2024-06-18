432d2c499b57b097d21189f79571b50fb733bd9c
Adhila10 <adhilaaboobakar10@gmail.com>
2024-06-18 10:13:50 +0530

    Added advanced function with percentage

--- calc.py
+++ calc.py
@@ -15,6 +15,9 @@ def divide(a, b):
     else:
         return a / b
 
+def percentage(a,b):
+    return a * b/100
+
 num1 = int(input("Enter num1"))
 num2 = int(input("Enter num2"))
 
@@ -22,3 +25,4 @@ print(f"{num1} + {num2} = {add(num1, num2)}")
 print(f"{num1} - {num2} = {subtract(num1, num2)}")
 print(f"{num1} * {num2} = {multiply(num1, num2)}")
 print(f"{num1} / {num2} = {divide(num1, num2)}")
+print(f"{num1} % of {num2} = {percentage(num1, num2)}")
